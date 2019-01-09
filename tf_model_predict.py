99629431b02169b61e1d6da80f70787b8854c587
Konstantin Kozlov <mackoel@gmail.com>
2019-01-09 09:48:26 +0300

    Fixed identation and removed erroneous futura data

--- tf_model_predict.py
+++ tf_model_predict.py
@@ -22,13 +22,13 @@ print(list_of_acc)
 
 for d in list_of_acc:
     print("using:", d)
-	model = load_model('blossum_model_' + d + '.h5')
+    model = load_model('blossum_model_' + d + '.h5')
     print(model.summary())
     df = df_full.loc[df_full['name'] == d]
     features = ['t_min', 't_max', 'dlen', 'day']
     data = df[features].values
     predicted_blossum = model.predict(data)
     df.insert(1, 'new', predicted_blossum)
-	df = df.loc[df['new'] >= 0.6]
-	df = df.loc[df.groupby('ID')['new'].idxmin()]
+    df = df.loc[df['new'] >= 0.6]
+    df = df.loc[df.groupby('ID')['new'].idxmin()]
     df.to_csv('futura_prediction_' + d + '.csv')
